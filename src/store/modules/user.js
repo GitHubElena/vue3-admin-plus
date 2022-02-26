8ceab97716bf52e3c9a44806fa115ad6ff97a0e8
liuchunfa <liuchunfa@siiture.com>
2022-02-26 11:09:33 +0800

    feat: 本地token存储和vuex存储

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,14 +1,23 @@
 import { loginApi } from '@/api/sys'
+import { setItem, getItem } from '@/utils/storage'
 import md5 from 'md5'
+import { TOKEN } from '@/constant'
 
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutation: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutation: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     /**
      * 登陆请求动作
-     * @param {*} context
+     * @param {*} context   {commit,state}
      * @param {*} userInfo
      * @returns
      */
@@ -19,6 +28,7 @@ export default {
           username,
           password: md5(password)
         }).then(data => {
+          context.commit('user/setToken', data.data.data.token)
           resolve()
         }).catch(err => {
           reject(err)
